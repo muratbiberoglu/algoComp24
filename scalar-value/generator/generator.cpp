6600fddd6e8a634f724c481c92a06f88bbade23f
fthbaskin <cs.fthbaskin@gmail.com>
2024-01-23 13:22:04 +0300

    need to think about tests

--- scalar-value/generator/generator.cpp
+++ scalar-value/generator/generator.cpp
@@ -7,6 +7,16 @@
 
 using namespace std;
 
+void check_connected(int node, unordered_map<int, vector<int>> &adj_list, unordered_map<int, bool> &visited)
+{
+    visited[node] = true;
+    for (auto neighbour : adj_list[node])
+    {
+        if (visited[neighbour] == false)
+            check_connected(neighbour, adj_list, visited);
+    }
+}
+
 vector<pair<int, int>> generatePossibleEulerianCircuit(int nodeCount, int edgeCount)
 {
     map<int, int> in_degrees;
@@ -91,15 +101,81 @@ vector<pair<int, int>> generateRandomGraph(int nodeCount, int edgeCount)
     return edges;
 }
 
-int main()
+string testGenerated(vector<pair<int, int>> edges)
 {
-    srand(time(NULL));
-    int n = 3;
-    int e = 3;
-    vector<pair<int, int>> edges = generatePossibleEulerianCircuit(n, e);
+    bool flag = true;
+    unordered_map<int, int> in_degrees;
+    unordered_map<int, int> out_degrees;
+    unordered_map<int, vector<int>> adj_list;
+    unordered_map<int, bool> visited;
 
     for (auto edge : edges)
     {
-        cout << edge.first << " " << edge.second << endl;
+        int u = edge.first;
+        int v = edge.second;
+        adj_list[u].push_back(v);
+        visited[u] = false;
+        visited[v] = false;
+        in_degrees[v]++;
+        out_degrees[u]++;
     }
+
+    for (auto in_degree : in_degrees)
+    {
+        int node = in_degree.first;
+        int degree = in_degree.second;
+        if (out_degrees[node] != degree)
+        {
+            flag = false;
+            break;
+        }
+    }
+
+    if (flag)
+    {
+        for (auto out_degree : out_degrees)
+        {
+            int node = out_degree.first;
+            int degree = out_degree.second;
+            if (in_degrees[node] != degree)
+            {
+                flag = false;
+                break;
+            }
+        }
+    }
+
+    if (flag)
+    {
+        if (in_degrees[1] == 0 || out_degrees[1] == 0)
+            flag = false;
+    }
+
+    // The graph might be disconnected
+    if (flag)
+    {
+        check_connected(1, adj_list, visited);
+        for (auto node : visited)
+        {
+            if (node.second == false)
+            {
+                flag = false;
+                break;
+            }
+        }
+    }
+
+    if (flag)
+        return "1";
+
+    return "-1";
+}
+
+int main()
+{
+    srand(time(NULL));
+    // NUM EDGES, NUM NODES, TYPE (1 - EULERIAN CIRCUIT, 2 - RANDOM GRAPH)
+    vector<tuple<int, int, int>> parameters = {
+        {3, 2, 1}};
+    return 0;
 }
